feac5fd8fc266319b97391d3b3e730f8fc7466cc
steven <12992@holbertonstudents.com>
2026-02-24 13:31:07 +0000

    hexa

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int n = 48;
+char l = 'a';
+
+while (n <= 57)
+{
+putchar(n);
+n++;
+}
+
+while (l <= 'f')
+{
+putchar(l);
+l++;
+}
+
+putchar('\n');
+
+return (0);
+}
