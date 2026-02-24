e87d4057ee44691d329bb6332b4720b5e17b489c
steven <12992@holbertonstudents.com>
2026-02-24 10:35:23 +0000

    alphABETS

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char l = 'a';
+char L = 'A';
+
+while (l <= 'z')
+{
+putchar(l);
+l++;
+}
+
+while (L <= 'Z')
+{
+putchar(L);
+L++;
+}
+
+putchar('\n');
+
+return (0);
+
+}
