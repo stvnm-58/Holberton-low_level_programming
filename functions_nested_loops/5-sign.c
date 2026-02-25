eabcd9660cf9caaa3b5a8806c7d5776288ec137f
steven <12992@holbertonstudents.com>
2026-02-25 15:30:34 +0000

    sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* _isalpha - checks for alphabetic character
+* @c: the character to check
+*
+* Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+*/
+
+int print_sign(int n)
+{
+if ( n > 0)
+{
+_putchar('+');
+return (1);
+}
+
+
+else if ( n == 0)
+{
+_putchar('0');
+return (0);
+}
+
+else
+{
+_putchar('-');
+return (-1);
+}
+}
