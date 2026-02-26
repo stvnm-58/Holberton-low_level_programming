4c8d29d667c37513bb7dc8b4f32b014a1019a18d
steven <12992@holbertonstudents.com>
2026-02-26 15:15:25 +0000

    multiplication

---
+++ intro_debugging/multplication_table.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void print_table(int size)
+{
+    int row, col;
+
+    for (row = 1; row <= size; row++)
+    {
+      
+        for (col = 1; col <= size; col++)
+        {
+            printf("%d ", row * col);
+            col++;
+            printf("%d ", row * col);
+        }
+        printf("\n");
+    }
+}
+
+/* -- DO NOT Modify the code below this line -- */
+
+int main(void)
+{
+    print_table(4);
+    return 0;
+}
