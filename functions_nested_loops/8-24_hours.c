f2ba703f4ffdc4814830928b41881b76ce6bc2ad
steven <12992@holbertonstudents.com>
2026-02-26 09:48:30 +0000

    24 hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* jack_bauer - prints every minute of the day of Jack Bauer
+*
+* Return: void
+*/
+
+void jack_bauer(void)
+{
+    int a, b, c, d;
+/**
+* a = heure dizaine
+* b = heure unité
+* c = minute dizaine
+* d = minute unité
+*/
+
+    a = 0;
+    while (a <= 2)
+    {
+        b = 0;
+        while (b <= 9)
+        {
+            if (a == 2 && b > 3)
+                break;
+
+            
+            
+            b++;
+        }
+        a++;
+    }
+}
