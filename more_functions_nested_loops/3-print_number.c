fcd63b7b3d41bd523e919ee42c0d8b73edb73f81
steven <12992@holbertonstudents.com>
2026-02-27 10:10:38 +0000

    print number

---
+++ more_functions_nested_loops/3-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - prints the numbers, from 0 to 9, followed by a new line
+*/
+void print_numbers (void)
+{
+    int a = 0
+    while (a <= 9)
+    {
+        _putchar (a);
+        a++
+        _putchar ("\n"); 
+    }
+_putchar ("\n")
+
+}
