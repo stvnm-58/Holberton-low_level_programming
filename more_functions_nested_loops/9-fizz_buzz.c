4f019e8822954ddda1fbd7b2e6aa97c6ffe959ce
steven <12992@holbertonstudents.com>
2026-02-28 12:48:01 +0000

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,23 +14,23 @@ int i;
 for (i = 1; i <= n ; i++)
 {
 
+if((i % 3 == 0) && (i % 5 == 0))
+{
+	printf("FizzBuzz ");
+}
 
-if ((i % 3 == 0) != (i % 5 == 0) )
+
+else if (i % 3 == 0)
 {
     printf("Fizz ");
 }
 
 
-
-else if((i % 5 == 0) != (i % 3 == 0))
+else if(i % 5 == 0)
 {
 	printf("Buzz ");
 }
 
-else if((i % 3 == 0) && (i % 5 == 0))
-{
-	printf("FizzBuzz ");
-}
 
 else
 {
